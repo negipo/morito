c5747829565a2a51d8e343db4542e30ee473d31b
negipo <negipo@gmail.com>
2014-06-13 20:45:37 +0900

    Discard cache with creating new client instance

--- lib/morito/client.rb
+++ lib/morito/client.rb
@@ -8,8 +8,29 @@ module Morito
 
     def allowed?(requesting_url, cache: true)
       uri = URI.parse(requesting_url)
-      robots_txt_body = Morito::Connector.new(uri, cache: cache).get
+      robots_txt_body = robots_txt_body(uri, cache: cache)
       Morito::Processor.new(robots_txt_body).allowed?(@user_agent, uri.path)
+    end
+
+    private
+
+    def robots_txt_body(uri, cache: true)
+      if cache
+        with_cache(uri.host) do
+          robots_txt_body_without_cache(uri)
+        end
+      else
+        robots_txt_body_without_cache(uri)
+      end
+    end
+
+    def robots_txt_body_without_cache(uri)
+      Morito::Connector.new(uri).get
+    end
+
+    def with_cache(host, &block)
+      @cache ||= {}
+      @cache[host] ||= yield
     end
   end
 end
--- lib/morito/connector.rb
+++ lib/morito/connector.rb
@@ -4,31 +4,15 @@ module Morito
   class Connector
     PATH = '/robots.txt'
 
-    def initialize(uri, cache: true)
+    def initialize(uri)
       @uri = uri
-      @cache = cache
     end
 
     def get
-      if @cache
-        self.class.with_cache(base_url) do
-          raw_get
-        end
-      else
-        raw_get
-      end
+      @get ||= connection.get(PATH).body
     end
 
     private
-
-    def self.with_cache(base_url, &block)
-      @cache ||= {}
-      @cache[base_url] ||= yield
-    end
-
-    def raw_get
-      @get ||= connection.get(PATH).body
-    end
 
     def connection
       Faraday.new(url: base_url) do |connection|
