9d1921cd82412741dcd8d4f8108f7a290dfb723a
negipo <negipo@gmail.com>
2014-06-13 20:20:01 +0900

    Add codeclimate-test-reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,14 @@
-require "simplecov"
+require 'simplecov'
 SimpleCov.start
 
 require 'webmock/rspec'
+
+if ENV['CI']
+  WebMock.disable_net_connect!(allow: %w(codeclimate.com))
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+end
+
 require 'morito'
 
 RSpec.configure do |config|
